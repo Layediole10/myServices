1f79f8554d57c34bfb2f3cff3f6f95bef76f80c9
abdoulaye diole <layediole88@gmail.com>
2022-08-20 03:18:49 +0200

    database ok

--- database/migrations/2022_08_19_213317_create_districts_table.php
+++ database/migrations/2022_08_19_213317_create_districts_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->unsignedBigInteger('municipality_id');
-            $table->foreign('municipality_id')->references('id')->on('municipalies');
+            $table->foreign('municipality_id')->references('id')->on('municipalities');
             $table->timestamps();
         });
     }
